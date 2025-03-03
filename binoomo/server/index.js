b5dfdab92b3aec3d5be2fd2af4efb9a4157fd2a5
Zendhel <zendhelros@gmail.com>
2025-03-03 18:14:02 -0500

    Fix package.json!

--- binoomo/server/index.js
+++ binoomo/server/index.js
@@ -2,8 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { testDatabase } from './config/database.js';
 
-
 dotenv.config();
+
+console.log(`host: ${process.env.DB_HOST},
+    port: ${process.env.DB_PORT},
+    user: ${process.env.DB_USER}, 
+    password: ${process.env.DB_PASSWORD},
+    database: ${process.env.DB_NAME}`)
 
 const app = express();
 
