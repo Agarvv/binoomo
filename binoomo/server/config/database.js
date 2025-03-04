3a95a7c8458e4e71dfa93ae67553f5c0df9683c5
Zendhel <zendhelros@gmail.com>
2025-03-03 20:43:37 -0500

    Connexion fixed

--- binoomo/server/config/database.js
+++ binoomo/server/config/database.js
@@ -2,12 +2,6 @@ import mysql from 'mysql2/promise';
 import { config } from './config.js';
 
 
-console.log("ENV Variables in database.js")
-console.log("DB_HOST:", process.env.DB_HOST ? "localhost" : "NOT SET");
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "******" : "NOT SET");
-console.log("DB_NAME:", process.env.DB_NAME);
-
 const pool = mysql.createPool({
   host: config.db.host,
   port: config.db.port,
--- binoomo/server/index.js
+++ binoomo/server/index.js
@@ -1,11 +1,6 @@
 import { config } from './config/config.js';
 import express from 'express';
 
-console.log("🔍 ENV Variables in index.js:");
-console.log("DB_HOST:", process.env.DB_HOST ? "localhost" : "NOT SET");
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "******" : "NOT SET");
-console.log("DB_NAME:", process.env.DB_NAME);
 
 import { testDatabase } from './config/database.js';
 
